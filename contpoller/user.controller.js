c368950a43bb47d3347284abdcfb4f00b7f7ba14
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-11-02 21:03:38 +0200

    update

--- contpoller/user.controller.js
+++ contpoller/user.controller.js
@@ -7,7 +7,6 @@ class UserControllers {
       `INSERT INTO person (name, surname) values($1, $2) RETURNING *`,
       [name, surname]
     );
-    // console.log(res.json(newPerson))
     res.json(newPerson.rows[0]);
   }
   async getUsers(req, res) {
@@ -16,21 +15,21 @@ class UserControllers {
   }
   async getOneUser(req, res) {
     const id = req.params.id;
-    const user = await db.query(`SELECT * FROM person were id = $1`, [id]);
-    res.json(newPerson.rows[0]);
+    const user = await db.query(`SELECT * FROM person where id = $1`, [id]);
+    res.json(user.rows[0]);
   }
   async updateUser(req, res) {
     const { id, name, surname } = req.body;
     const user = await db.query(
-      "UPDATE person set name = $1, surname = $2, id = $3 RETURNING *",
+      'UPDATE person set name = $1, surname = $2, id = $3 RETURNING *',
       [name, surname, id]
     );
     res.json(user.rows[0]);
   }
   async deleteUser(req, res) {
     const id = req.params.id;
-    const user = await db.query(`DELETE FROM person were id = $1`, [id]);
-    res.json(newPerson.rows[0]);
+    const user = await db.query('DELETE FROM person where id = $1', [id]);
+    res.json(user.rows[0]);
   }
 }
 
