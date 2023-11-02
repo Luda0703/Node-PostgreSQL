2fde98f201a6dde991896add996599cbb1f524a8
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-11-03 01:54:26 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const userRouter = require("./routes/user.routes");
 const gamerRouter = require("./routes/gamer.Controller");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api", userRouter);
 app.use("/api", gamerRouter);
 
